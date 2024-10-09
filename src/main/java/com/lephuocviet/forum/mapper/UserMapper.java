cfb1ed9bd4de5ac8f07c273ff370be68603ba6c3
LePhuocViet <139203809+LePhuocViet@users.noreply.github.com>
2024-10-09 18:47:38 +0700

    UserService with mapper 2

--- src/main/java/com/lephuocviet/forum/mapper/UserMapper.java
+++ src/main/java/com/lephuocviet/forum/mapper/UserMapper.java
@@ -14,12 +14,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "language", target = "language")
-    @Mapping(source = "sex", target = "sex")
-    @Mapping(target = "img", ignore = true)
-    @Mapping(source = "email", target = "email")
-    Users toUser(UserRequest userRequest);
+//    @Mapping(source = "name", target = "name")
+//    @Mapping(source = "language", target = "language")
+//    @Mapping(source = "sex", target = "sex")
+//    @Mapping(target = "img", ignore = true)
+//    @Mapping(source = "email", target = "email")
+//    Users toUser(UserRequest userRequest);
 
 
     UserResponse toUserResponses(Users users);
